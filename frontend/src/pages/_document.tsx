8480347f5e2c61632f2b88510a586e1bdbebe430
chadharsahib01 <casualloginn@gmail.com>
2025-06-17 16:07:55 +0500

    Update _document.tsx

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -2,9 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" data-theme="dark">
-      <Head />
-      <body className="scroll-smooth">
+    <Html lang="en">
+      <Head>
+        <title>MyPakistanFunds</title>
+        <meta name="description" content="Track and analyze Pakistan mutual funds" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
